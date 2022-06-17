d569d4e0861e0b3eab15eb937ae27565a32b0ff9
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 10:24:39 +0300

    Update ex26.go

--- ex26/ex26.go
+++ ex26/ex26.go
@@ -9,6 +9,7 @@ func uniqueSymbol(str string) bool {
 	temp := []rune(strings.ToLower(str))
 	mmap := make(map[rune]bool)
 	
+	// мапим каждый символ, пока он не встретится в очередной раз
 	for _, k := range temp {
 		if _, ok := mmap[k]; ok {
 			return false
@@ -18,6 +19,7 @@ func uniqueSymbol(str string) bool {
 	return true
 }
 
+// можно решить за n^2 делая полный перебор
 func main() {
 	var str string
 	fmt.Println("Enter any strings")
