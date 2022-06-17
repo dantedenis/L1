6998e361e819c92afa69d37c5514c633fe2ee0b1
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 09:51:48 +0300

    Update ex14.go

--- ex14/ex14.go
+++ ex14/ex14.go
@@ -16,10 +16,12 @@ func main() {
 	DetectTypePrintf(make(chan int))
 }
 
+// определение типа с помощью пакета рефлект
 func DetectType(val interface{}) {
 	fmt.Println("Type:", reflect.TypeOf(val))
 }
 
+// можно просто принтф, но он так же использует рефлексию
 func DetectTypePrintf(val interface{}) {
 	fmt.Printf("_Type: %T\n", val)
 }
