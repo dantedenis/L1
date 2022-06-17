07df7c9c16427e25d7a1b6199ae2e13015da1812
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 10:15:16 +0300

    Update ex24.go

--- ex24/ex24.go
+++ ex24/ex24.go
@@ -4,6 +4,7 @@ import (
 	"math"
 )
 
+// структура поинт с неэкспортируемыми полями
 type Point struct {
 	x, y float64
 }
@@ -12,12 +13,14 @@ func NewPoint(x, y float64) *Point {
 	return &Point{x: x, y: y}
 }
 
+// метод структуры для нахождения дистанции
 func (p Point) Distance(end Point) float64 {
 	difX := math.Pow(p.x-end.x, 2)
 	difY := math.Pow(p.y-end.y, 2)
 	return math.Sqrt(difX + difY)
 }
 
+//экспортируемый метод от 2 поинтов
 func Distance(first, second Point) float64 {
 	return first.Distance(second)
 }
