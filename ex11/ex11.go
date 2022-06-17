d3767a9e84297f477e5fa1e72533f4fdb6c848f5
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 09:45:57 +0300

    add comments

--- ex11/ex11.go
+++ ex11/ex11.go
@@ -4,10 +4,12 @@ func GetInter[T comparable](first, second []T) []T {
 	result := make([]T, 0)
 	myMap := make(map[T]bool)
 
+	// просто мапим каждое значение из первого слайса
 	for _, t := range first {
 		myMap[t] = true
 	}
 
+	// проверяем на совпадение значений в мапе и 2го слайса -> добавляем их в новый слайс
 	for _, t := range second {
 		if _, ok := myMap[t]; ok {
 			result = append(result, t)
@@ -16,3 +18,5 @@ func GetInter[T comparable](first, second []T) []T {
 
 	return result
 }
+
+// Также можно решить за O(n^2): сравнив попарно все значения в 2х слайсах
