8531669bedf174b0e47839dabb97fe5543e9f767
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 10:12:50 +0300

    Update ex20.go

--- ex20/ex20.go
+++ ex20/ex20.go
@@ -5,9 +5,11 @@ import (
 )
 
 func ReverseWords(text string) string {
+	// преобразуем текст в массив, что бы элементы поменять местами
 	words := strings.Split(text," ")
 	for i,j := 0, len(words) - 1; i < j; i, j = i + 1, j - 1 {
 		words[i], words[j] = words[j], words[i]
 	}
+	// конкатенируем слова в строку
 	return strings.Join(words, " ")
 }
