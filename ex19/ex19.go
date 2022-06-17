e088582d461d165d3ac3d0ad7c14e5bd1891257f
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 10:11:30 +0300

    Update ex19.go

--- ex19/ex19.go
+++ ex19/ex19.go
@@ -1,6 +1,6 @@
 package ex19
 
-
+// достаточно бежать посимвольно и свапать их местами (метод 2 указателей)
 func ReversString(str string) string {
 	runes := []rune(str)
 	for i,j := 0, len(runes) -1; i < j; i, j = i + 1, j - 1 {
