a7e32306ab35628ea90d012f329f8bfbd285f39e
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 09:35:04 +0300

    add comments

--- ex10/ex10.go
+++ ex10/ex10.go
@@ -1,9 +1,12 @@
 package ex10
 
 func TemperatureGroup(temps []float64) map[int][]float64 {
+	// инициализируем мапу
 	result := make(map[int][]float64)
 	for _, t := range temps {
+		// делим на группы по сонованию 10
 		group := int(t/10) * 10
+		// сохраняем в мапе в необходимой группе
 		result[group] = append(result[group], t)
 	}
 	return result
