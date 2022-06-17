baebc2db4048bfa5acd786e630a83e65f0c9a194
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 10:00:20 +0300

    Update ex13.go

--- ex13/ex13.go
+++ ex13/ex13.go
@@ -1,9 +1,11 @@
 package ex13
 
+// свап за счет "синтаксического сахара" го
 func Swap(first, second *int) {
 	*first, *second = *second, *first
 }
 
+// просто с помощью 2х сумм и разности
 func SwapSum(first, second *int) {
 	*first += *second
 	*second = *first - *second
