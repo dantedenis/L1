4676ab4e0963d5b40f96feed59234a19423b461d
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 10:20:47 +0300

    Update ex25.go

--- ex25/ex25.go
+++ ex25/ex25.go
@@ -17,17 +17,19 @@ func main() {
 	fmt.Println(time.Now())
 }
 
-
+// Ожидание с помощью метода After, пока не вернет значени в канал через t время
 func SleepWithAfter(t time.Duration) {
 	<- time.After(t)
 }
 
+// Ожидание с помощью контекста с таймаутом t
 func SleepWithCtx(t time.Duration) {
 	end, cancel := context.WithTimeout(context.Background(), t)
 	defer cancel()
 	<- end.Done()
 }
 
+// Цикл с проверкой прошло ли достаточно кол-ва времени t или нет
 func SleepWithLoop(t time.Duration) {
 	start := time.Now()
 	
@@ -39,6 +41,7 @@ func SleepWithLoop(t time.Duration) {
 	}
 }
 
+// слип с помощью тика, который пишет в канал после t времени
 func SleepWithTick(t time.Duration) {
 	ticks := time.Tick(t)
 	<- ticks
