c5c53689a09c49667e6180fc67eb83e9aefaf16d
Denis <76144861+dantedenis@users.noreply.github.com>
2022-06-17 09:11:33 +0300

    add comments

--- ex06/ex06.go
+++ ex06/ex06.go
@@ -23,12 +23,15 @@ func ChanStop() {
 	quit := make(chan bool)
 	go func() {
 		fmt.Println("Start goroutine")
+		//инструкции перед завершением грутины
 		defer func() {
+			close(quit)
 			fmt.Println("Stop goroutine")
 		}()
 
 		for {
 			select {
+			// Завершение горутины по принятию флагу в канал
 			case <-quit:
 				return
 			}
@@ -36,13 +39,14 @@ func ChanStop() {
 	}()
 
 	fmt.Println("Send true flag")
-
+	// Отправка сигнала на завершение горутины
 	quit <- true
 }
 
 func SignalStop() {
 	fmt.Println("\tSignal Stop:")
 	sigChan := make(chan os.Signal, 1)
+	// сам метод и горутина работаю, пока не будет принят сигнял прерывания
 	signal.Notify(sigChan, syscall.SIGINT)
 	go func() {
 		for {
@@ -50,6 +54,7 @@ func SignalStop() {
 			time.Sleep(1 * time.Second)
 		}
 	}()
+	// ожидание сигнала прерывания
 	<-sigChan
 	fmt.Println("Stop goroutine")
 }
@@ -60,12 +65,13 @@ func WaitGroupStop() {
 		fmt.Println("Stop goroutine")
 	}()
 	wg := sync.WaitGroup{}
+	// Группа горутин, завершение которых будем ожидать, в количестве 1шт
 	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		for i := 0; i < 10; i++ {
 			fmt.Println("Working...")
 		}
-		wg.Done()
 	}()
 	wg.Wait()
 }
@@ -78,24 +84,50 @@ func TimeoutStop() {
 			time.Sleep(500 * time.Millisecond)
 		}
 	}()
+	// Завершение метода вместе с горутиной по таймеру
 	select {
 	case <-time.After(2 * time.Second):
 		fmt.Println("Stop goroutine")
 	}
 }
 
+func TimeoutStop2() {
+	fmt.Println("\tTimeout Stop:")
+	// Инициализация групп, для ожидания их завершения выполнения
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			// Завершение горутины по таймеру
+			select {
+			case <-time.After(2 * time.Second):
+				fmt.Println("Stop goroutine")
+			default:
+				fmt.Println("Working...")	
+			}
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+	wg.Wait()
+}
+
 func ContextStop() {
 	fmt.Println("\tContext Stop:")
+	//Инициализация контекста с таймаутом, для завершения горутины по превышении времени выполнения
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
+		defer func(){ 
+			wg.Done()
+			fmt.Println("Stop goroutine")
+		}
 		for {
 			select {
+			// Таймаут от контекста, завершение горутины
 			case <-ctx.Done():
-				fmt.Println("Stop goroutine")
-				wg.Done()
 				return
 			default:
 				fmt.Println("Working...")
